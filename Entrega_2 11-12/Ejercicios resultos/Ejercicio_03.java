f75f8804d9215a068b07baa6838e3e5a17f1b3ee
Agustin Acevedo <arustis.99@gmail.com>
2023-12-11 22:29:36 -0300

    Entrega de todos los ejercicios

---
+++ Entrega_2 11-12/Ejercicios resultos/Ejercicio_03.java
@@ -0,0 +1,14 @@
+package com.educacionIT.desafio2;
+
+public class Ejercicio_03 {
+
+	public static void main(String[] args) {
+		int n1 = 10, n2 = 20, n3 = 30;
+		System.out.println("n1=" + n1 + " n2=" + n2 + " n3=" + n3);
+		System.out.println("La suma total de n1, n2 y n3 es= " + (n1 + n2 + n3));
+		System.out.println("El promedio entre n1, n2 y n3 es= " + ((n1 + n2 + n3) / 3));
+		System.out.println("El resto entre n2 y n1 es= " + (n2 % n1));
+
+	}
+
+}
