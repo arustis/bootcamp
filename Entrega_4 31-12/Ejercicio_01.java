219f7e4d481c538b19350f773b6ae357a2b16323
Agustin Acevedo <arustis.99@gmail.com>
2024-01-03 23:23:57 -0300

    Actualizacion de ejercicio 01

--- Entrega_4 31-12/Ejercicio_01.java
+++ Entrega_4 31-12/Ejercicio_01.java
@@ -5,44 +5,41 @@ import java.util.Scanner;
 public class Ejercicio_01 {
 
 	public static void main(String[] args) {
-		//declaro las variables a utilizar, el array donde guardo los numeros lo defino de una longitud de 100
-		int posicion = 0;
+		// declaro las variables a utilizar
 		int resultado = 0;
+		int numeroLeido = 0;
 		int mayor = 0;
 		int menor = 0;
-		int[] numerosIngresados = new int[100];
-		//inicializo el scanner para que el usuario ingrese los datos por consola
+		// inicializo el scanner para que el usuario ingrese los datos por consola
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Bienvenido al programa de suma de numeros");
-		
-		while (true) {
-			//le solicito al usuario que ingrese el numero por la consola
+		// inicio el ciclo do-while, donde consulto si se ingreso el numero 0 al final
+		// del ciclo
+		do {
+			// le solicito al usuario que ingrese el numero por la consola
 			System.out.print("Para finalizar con el programa ingrese 0, de lo contrario escriba cualquier numero: ");
-			//guardo el numero en el array en la posicion actual
-			numerosIngresados[posicion] = scanner.nextInt();
-			//luego consulto si se ingreso 0, si es asi, sumo los valores, adicionalmente obtengo los numeros mayor y menor
-			if (numerosIngresados[posicion] == 0) {
-				for (int i = 0; i < posicion; i++) {
-					resultado = resultado + numerosIngresados[i];
-
-				}
-				mayor=numerosIngresados[0];
-				for (int i = 0; i < posicion; i++) {
-					if (numerosIngresados[i] > mayor)
-						mayor = numerosIngresados[i];
-
-				}
-				menor = mayor;
-				for (int i = 0; i < posicion; i++) {
-					if (numerosIngresados[i] < menor)
-						menor = numerosIngresados[i];
-
-				}
-				break;
+			// sumo el numero ingresado al resultado final
+			numeroLeido = scanner.nextInt();
+			resultado = resultado + numeroLeido;
+			// cuando iniliza el programa con las variables en 0, asigno el valor a mayor y
+			// menor
+			if (mayor == 0) {
+				mayor = numeroLeido;
 			}
-			posicion++;
-		}
-		//Muestro los resultados y los numeros mayor y menor por consola
+			if (menor == 0) {
+				menor = numeroLeido;
+			}
+			// luego voy comprando con cada valor ingresado si este es mayor o menor al que
+			// ya estaba guardado
+			if (mayor < numeroLeido && numeroLeido != 0) {
+				mayor = numeroLeido;
+			}
+			if (menor > numeroLeido && numeroLeido != 0) {
+				menor = numeroLeido;
+			}
+
+		} while (numeroLeido != 0);
+		// Muestro los resultados y los numeros mayor y menor por consola
 		System.out.println("La sum total de los numeros es: " + resultado);
 		System.out.println("El numero mayor es: " + mayor);
 		System.out.println("El numero menor es: " + menor);
